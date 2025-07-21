446b5692b7627f929ee82065f7b9fdae11c0888b
Marperette <littra@gmail.com>
2025-07-21 14:59:45 +0200

    search function
    Added code to create some sort of search function/functionality for autocomplete. Also added materialUI for the searchfield

--- movie-search/src/app/fetchData.tsx
+++ movie-search/src/app/fetchData.tsx
@@ -1,23 +1,35 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import useFetch from "react-fetch-hook";
 
+interface movie {
+    id: string;
+    name: string;
+    description: string;
+}
 
-const FetchData = () => {
+const FetchData = (search:string) => {
        const [movies, setMovies] = useState([]);
        useEffect(()=> {
-        fetch('https://movies-mock-api-677053851485.europe-north1.run.app/api/movies').then((res)=> {
+        console.log("entered effect")
+        fetch(`https://movies-mock-api-677053851485.europe-north1.run.app/api/movies?q=${search.search}`).then((res)=> {
             return res.json();
         }).then((data) => {
             console.log(data);
             setMovies(data);
         });
-       }, []);
-       return (
-        <div>
-            {movies.map((movie) => (<p>{movie.name}</p>))}
-        </div>
-       )
+       }, [search]);
+       console.log(search);
+
+       
+        return (
+            <div>
+                {movies.map((movie : movie) => (<p>{movie.name}</p>))}
+            </div>
+           )
+       
     }
+
+    //Är det fel att lägga in mer skaer här, borde jag låta den vara en ren fetch och att den bara retunerar movies som kan hämtas av en annan component.
+    //Innebär detta att jag för varje sökning hämtar all data igen? Inte det mest optimala med stora mängder data, då är det bättre att ha det sparat i cache på något vis
 export default FetchData;
--- movie-search/src/app/page.tsx
+++ movie-search/src/app/page.tsx
@@ -1,15 +1,29 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import FetchData from "./fetchData";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+
+
 export default function Home() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e: { target: { value: string; }; }) => {
+    let lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  }
   return (
     <div>
       <h1>Movie Search</h1>
-      <textarea></textarea>
-      <button>Search</button>
+      <TextField
+      id="outlined-basic"
+      variant="outlined"
+      label="Search"
+      onChange={inputHandler} />
       <div>
         <h2>Results:</h2>
-        <FetchData/>
+        <FetchData search={inputText}/>
       </div>
     </div>
   );
